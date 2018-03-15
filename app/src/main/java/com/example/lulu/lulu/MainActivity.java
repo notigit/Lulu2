f912ac597f6d8d86f2c325bab3a3d6fa01d312a2
tanguowei <Yu6R5c76>
2018-03-15 16:19:21 +0800

    3

--- app/src/main/java/com/example/lulu/lulu/MainActivity.java
+++ app/src/main/java/com/example/lulu/lulu/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.lulu.lulu;
 
+import android.os.Build;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.View;
-import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import java.io.IOException;
 
@@ -21,17 +22,51 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = (WebView) findViewById(R.id.webView);
 //        webView.loadUrl("http://juju.011pp.com:11116/");
-        webView.loadUrl("http://video.sina.com.cn/p/news/o/doc/2018-03-04/210168059742.html");
-        WebSettings webSettings = webView .getSettings();
+        final String url = "http://ask.csdn.net/questions/178242";
+        webView.loadUrl(url);
+        WebSettings webSettings = webView.getSettings();
         webSettings.setDefaultTextEncodingName("UTF-8");
         webSettings.setJavaScriptEnabled(true); //支持js
         webSettings.setAllowFileAccess(true); // 允许访问文件
+        //如果不设置WebViewClient，请求会跳转系统浏览器
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //该方法在Build.VERSION_CODES.LOLLIPOP以前有效，从Build.VERSION_CODES.LOLLIPOP起，建议使用shouldOverrideUrlLoading(WebView, WebResourceRequest)} instead
+                //返回false，意味着请求过程里，不管有多少次的跳转请求（即新的请求地址），均交给webView自己处理，这也是此方法的默认处理
+                //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://ask.csdn.net/questions/178242
+
+                if (url.toString().contains("sina.cn")) {
+                    view.loadUrl(url);
+                    return true;
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                //返回false，意味着请求过程里，不管有多少次的跳转请求（即新的请求地址），均交给webView自己处理，这也是此方法的默认处理
+                //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://ask.csdn.net/questions/178242
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    if (request.getUrl().toString().contains("sina.cn")) {
+                        view.loadUrl(url);
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+        });
         //支持全屏
 //        webView.setWebChromeClient(new WebChromeClient(){
 //            @Override
@@ -47,27 +82,28 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-         String androidId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        String androidId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
 
         Log.e("TAG", "ANDROID_ID: " + androidId);
 //        getOnlyId(this);
 //        549614307f56fd39
 //        7ae6287337d63b6a
         String channel = ChannelUtils.getChannel(this);
-        Log.e("TAG", "CHANNEL: "+channel);
-        postDataWithParame(channel,androidId);
+        Log.e("TAG", "CHANNEL: " + channel);
+        postDataWithParame(channel, androidId);
     }
 
     /**
      * 安装量统计
+     *
      * @param channel
      * @param androidId
      */
     private void postDataWithParame(String channel, String androidId) {
         OkHttpClient client = new OkHttpClient();//创建OkHttpClient对象。
         FormBody.Builder formBody = new FormBody.Builder();//创建表单请求体
-        formBody.add("channel",channel);//渠道名
-        formBody.add("mac",androidId);//唯一标识
+        formBody.add("channel", channel);//渠道名
+        formBody.add("mac", androidId);//唯一标识
         Request request = new Request.Builder()//创建Request 对象。
                 .url("http://ceshi.com/api/channel.php")
                 .post(formBody.build())//传递请求体
@@ -76,18 +112,19 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.e("TAG", "onFailure: " );
+                Log.e("TAG", "onFailure: ");
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                Log.e("TAG", "onResponse: "+response.code() );
+                Log.e("TAG", "onResponse: " + response.code());
             }
         });//回调方法的使用与get异步请求相同，此时略。
     }
 
     /**
      * 改写物理按键返回的逻辑
+     *
      * @param keyCode
      * @param event
      * @return
